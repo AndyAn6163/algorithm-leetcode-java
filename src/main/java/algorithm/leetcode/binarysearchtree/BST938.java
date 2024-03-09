1f5ec894376cb04895dbb81a4ce5ce71471f29c2
AndyAn6163 <lanews580@gmail.com>
2024-03-09 16:48:34 +0800

    feat: initial commit

---
+++ src/main/java/algorithm/leetcode/binarysearchtree/BST938.java
@@ -0,0 +1,8 @@
+package algorithm.leetcode.binarysearchtree;
+
+public class BST938 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/algorithm/leetcode/binarytree/BT94.java
@@ -0,0 +1,4 @@
+package algorithm.leetcode.binarytree;
+
+public class BT94 {
+}
